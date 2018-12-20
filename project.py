c1717c000748f356cac97af7a4e87fec2b06a5bf
Vitaliy <34397062+theslvr@users.noreply.github.com>
2018-12-20 17:12:31 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,8 +1,8 @@
 A = int(input())
 
 if A % 100 == 0 and A % 400 != 0:
-    print('Обычный')
+    print('usual')
 elif A % 400 == 0 or A % 4 == 0:
-    print('Високосный')
+    print('leap')
 else:
-    print('Обычный')
+    print('usual')
